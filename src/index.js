9990162822b0a97abfec9c54369fb6055b9696c2
SrRequiem <theanondebian@gmail.com>
2020-01-13 18:34:02 -0600

    Ready to start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from "firebase";
+import firebase from "firebase/app";
 import config from "./firebase";
 
 firebase.initializeApp(config);
